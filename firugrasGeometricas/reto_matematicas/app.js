1b2b3ad8d989f15c2d06b2f0d04691983f47d823
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-06-14 08:01:00 -0500

    ejercicio completado

---
+++ firugrasGeometricas/reto_matematicas/app.js
@@ -0,0 +1,49 @@
+
+var calcularAlturaTrianguloIsoceles = (lado1, lado2, base) => {
+    if(lado1 === lado2 && lado1 != base) {
+        return Math.sqrt((lado2**2) - ((base / 2)**2));
+    } else {
+
+        console.log("No es un triangulo isoceles");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
